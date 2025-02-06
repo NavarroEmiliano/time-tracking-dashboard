fd668c1888f1cb4e06c990f1224ddbb3050fabee
Emiliano Navarro <emilianonavarro1993@gmail.com>
2025-02-06 02:18:24 -0300

    javascript

--- index.js
+++ index.js
@@ -1,40 +1,66 @@
-let fetchData;
 const mainContainer = document.querySelector(".main-container");
 
 fetch("/data.json")
   .then((response) => {
-    if (!response.ok) return "failed to fetch";
+    if (!response.ok) throw new Error("Failed to fetch");
     return response.json();
   })
   .then((data) => {
-    data.forEach((item) => {
-      appendItem(item);
-    });
+    populateDOM(data);
+  })
+  .catch((error) => console.error(error));
+
+const populateDOM = (data) => {
+  const fragment = document.createDocumentFragment();
+  
+  data.forEach((item) => {
+    const newItem = createItemElement(item);
+    fragment.appendChild(newItem);
   });
 
-const appendItem = (item) => {
-  const itemTitle = item.title.toLowerCase().split(" ").join("-");
-
-  mainContainer.innerHTML += `
-                <div class="time-box">
-      <div class="time-box__image-container time-box__image-container--${itemTitle}">
-        <img src="/images/icon-${itemTitle}.svg" alt="">
-      </div>
-
-
-      <div class="time-box__time">
-        <div class="time-box__title-and-dots">
-          <p class="time-box__title">${item.title}</p>
-          <img class="time-box__dots" src="/images/icon-ellipsis.svg" alt="">
-        </div>
-        <div class="time-box__hours-and-prev">
-          <p class="time-box__hours">${item.timeframes.daily.current}hrs</p>
-          <p class="time-box__prev"> Previous - ${item.timeframes.daily.previous}hrs</p>
-        </div>
-      </div>
-
-    </div>
-        `;
+  mainContainer.appendChild(fragment);
 };
 
-console.log(fetchData);
+const createItemElement = (item) => {
+  const itemTitle = item.title.toLowerCase().split(" ").join("-");
+
+  const timeBox = document.createElement("div");
+  timeBox.classList.add("time-box");
+
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("time-box__image-container", `time-box__image-container--${itemTitle}`);
+  const image = document.createElement("img");
+  image.src = `/images/icon-${itemTitle}.svg`;
+  image.alt = `${item.title} icon`;
+  imageContainer.appendChild(image);
+
+  const timeInfo = document.createElement("div");
+  timeInfo.classList.add("time-box__time");
+
+  const titleAndDots = document.createElement("div");
+  titleAndDots.classList.add("time-box__title-and-dots");
+  titleAndDots.innerHTML = `
+    <p class="time-box__title">${item.title}</p>
+    <img class="time-box__dots" src="/images/icon-ellipsis.svg" alt="ellipsis icon">
+  `;
+
+  const hoursAndPrev = document.createElement("div");
+  hoursAndPrev.classList.add("time-box__hours-and-prev");
+  hoursAndPrev.innerHTML = `
+    <p class="time-box__hours">${item.timeframes.daily.current}hrs</p>
+    <p class="time-box__prev">Previous - ${item.timeframes.daily.previous}hrs</p>
+  `;
+
+  timeInfo.appendChild(titleAndDots);
+  timeInfo.appendChild(hoursAndPrev);
+  timeBox.appendChild(imageContainer);
+  timeBox.appendChild(timeInfo);
+
+  return timeBox;
+};
+
+document.addEventListener("click", (event) => {
+  if (event.target.matches(".user-box__button")) {
+    console.log(`Se hizo clic en: ${event.target.textContent}`);
+  }
+});
